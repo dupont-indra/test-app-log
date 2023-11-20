6181b8311f207c8fd3381e69b612a1e5d3e094eb
dupont-indra <92726998+dupont-indra@users.noreply.github.com>
2023-11-20 22:32:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def print_log():
-  for i in range(1,10):
-    print(f'Logging : {i}')
+from typing import Union
 
-print_log()
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    print(f'Returning Hello World')
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
