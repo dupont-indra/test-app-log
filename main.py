c774e0676d96612faacc151a44d1fe6a30ccfd07
dupont-indra <92726998+dupont-indra@users.noreply.github.com>
2023-11-20 22:32:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    print(f'Item ID : {item_id} and q : {q}')
     return {"item_id": item_id, "q": q}
