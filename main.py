216aa9d8ca77c944676ebd38c9569145fd59225e
dupont-indra <92726998+dupont-indra@users.noreply.github.com>
2023-11-21 19:56:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from typing import Union
 
 from fastapi import FastAPI
+import logging
 
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    print(f'Returning Hello World')
+    logger.debug(f'Returing Hello World')
+    print(f'This is a print hello world')
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    print(f'Item ID : {item_id} and q : {q}')
+    logger.info(f'Info Log : {item_id}')
+    logger.error(f'Error Log ')
     return {"item_id": item_id, "q": q}
